eb654a18484aa31c254a5bb13c430027ef52de81
Bojan Stanimirovski <bojan_stanimirovski097@hotmail.com>
2022-11-03 18:07:51 +0100

    Class 07

--- Class 07/handlers/storage.js
+++ Class 07/handlers/storage.js
@@ -1,15 +1,19 @@
 const upload = async (req, res) => {
   const path = require("path");
 
-  const extRegex = new RegExp(/^(\.png|\.jpg)$/);
+  // const extRegex = new RegExp(/^(\.png|\.jpg)$/);
 
-  // if (path.extname(req.files.slika.name).toLowerCase() !== ['.png'||".jpg"])
-  // if (path.extname(req.files.slika.name).toLowerCase() !== extRegex)
-  if (path.extname(req.files.slika.name).toLowerCase() !== ".jpg") {
-    res.status(406).contentType("text/plain").end("Format is Not Acceptable");
-  } else {
+  // if (path.extname(req.files.slika.name).toLowerCase() === ['.png'||".jpg"])
+  // if (path.extname(req.files.slika.name).toLowerCase() === extRegex)
+  // if (path.extname(req.files.slika.name).toLowerCase() === ".jpg")
+
+  const imgTypes = [".jpg", ".png", ".ico", ".jpeg"];
+
+  if (path.extname(req.files.slika.name).toLowerCase() === imgTypes[0]) {
     req.files.slika.mv(`${__dirname}/../uploads/${req.files.slika.name}`);
     res.status(200).contentType("text/plain").end("File uploaded!");
+  } else {
+    res.status(406).contentType("text/plain").end("Format is Not Acceptable");
   }
 };
 
